5ca890c38695ced1e6a6d514f114ea8a0e4b85d2
Aluminium95 <lopezaliaume@gmail.com>
2011-10-26 17:39:43 +0200

    Ajout des scripts de menu en lua

---
+++ Data/Menu/start.lua
@@ -0,0 +1,5 @@
+x = getWidth () -- Récupère la largeur de l'écran
+
+bouton (x/3 - 100,40,"Quitter.png", "QUITTER") -- Ajoute le bouton quitter
+bouton (x/3 * 2 -100, 40, "Commencer.png", "COMMENCER") -- Ajoute le bouton commencer
+bouton (x/2 -100, 200, "Resume.png", "CONTINUER") -- Ajoute le bouton continuer
