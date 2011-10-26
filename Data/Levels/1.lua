10f5f7c48671a44a766b5ca2cda869997979901c
Aluminium95 <lopezaliaume@gmail.com>
2011-10-26 11:17:31 +0200

    Ajout du level 1 en tant que script !

---
+++ Data/Levels/1.lua
@@ -0,0 +1,5 @@
+addTerrain (60,30,20) -- Ajoute un terrain !
+addTerrain (70,20,50) -- Ajoute un deuxième terrain !
+
+addIA (30,10,"Caca") -- Ajoute une IA !
+addJoueur (50,50, "Joueur 1") -- Ajoute un Joueur !
