ab72effeb1e90b7995a7cdbd908c96e0d990c8f2
Aluminium95 <lopezaliaume@gmail.com>
2011-10-26 11:35:03 +0200

    Modification mineures level 1

--- Data/Levels/1.lua
+++ Data/Levels/1.lua
@@ -1,8 +1,13 @@
-terrain (60,30,20) -- Ajoute un terrain !
-terrain (70,20,50) -- Ajoute un deuxième terrain !
-terrain (150,50,150) -- Ajoute un troisième terrain !
-terrain (29,150,20)
-terrain (200,20, 25)
+terrain (60, 30, 20) -- Ajoute un terrain !
+terrain (70, 20, 50) -- Ajoute un deuxième terrain !
+terrain (150, 50, 150) -- Ajoute un troisième terrain !
+terrain (29, 150, 20)
+terrain (200, 20, 25)
+terrain (2, 25, 150)
+terrain (200, 150, 40)
 
 ia (30,10,"Caca") -- Ajoute une IA !
 joueur (50,50, "Joueur 1") -- Ajoute un Joueur !
+
+-- Il faut faire un système pour gèrer la taille de l'écran !
+-- Ou alors centrer les terrains ?
